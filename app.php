8bc66680e5d4bd54b399853bb28ff86068a41d33
basavaraj-nanjappa <127355458+basavaraj-nanjappa@users.noreply.github.com>
2024-12-30 19:16:29 +0530

    Hotfix commit 2

--- app.php
+++ app.php
@@ -13,7 +13,12 @@ $version = "1.1.1";
     <h2>This is version <?php echo $version ?></h2>
     <ol>
         <li>initial commit</li>
-            <li>hotfix-SW-x01</li>
+            <li>hotfix-SW-x01
+                <ol>
+                    <li>commit 01</li>
+                    <li>commit 02</li>
+                </ol>
+            </li>
         <li>integration-rel-r01
             <ol>
                 <li>epic-01
