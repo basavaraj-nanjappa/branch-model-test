e62135004a7f2d59d96aadff6bae34d4c748ce4a
basavaraj-nanjappa <127355458+basavaraj-nanjappa@users.noreply.github.com>
2024-12-30 21:16:34 +0530

    resolved conflicts and corrected after resolving

--- app.php
+++ app.php
@@ -13,12 +13,21 @@ $version = "1.1.1";
     <h2>This is version <?php echo $version ?></h2>
     <ol>
         <li>initial commit</li>
-            <li>hotfix-SW-x01
-                <ol>
-                    <li>commit 01</li>
-                    <li>commit 02</li>
-                </ol>
-            </li>
+        <li>hotfix-SW-x01
+            <ol>
+                <li>commit 01</li>
+                <li>commit 02</li>
+            </ol>
+        </li>
+        <li>feat-sw-df01
+            <ol>
+                <li>commit 01</li>
+                <li>commit 02</li>
+                <li>commit 03</li>
+                <li>commit 04</li>
+                <li>commit 05</li>
+            </ol>
+        </li>
         <li>integration-rel-r01
             <ol>
                 <li>epic-01
@@ -40,32 +49,6 @@ $version = "1.1.1";
                 </li>
             </ol>
         </li>
-            <li>feat-sw-df01
-                <ol>
-                    <li>commit 01</li>
-                    <li>commit 02</li>
-                    <li>commit 03</li>
-                    <li>commit 04</li>
-                    <li>commit 05</li>
-                </ol>
-            </li>
-            <li>epic-01
-                <ol>
-                    <li>feat-sw-e01-f01
-                        <ol>
-                            <li>commit 01</li>
-                            <li>commit 02</li>
-                            <li>commit 03</li>
-                        </ol>
-                    </li>
-                    <li>feat-sw-e01-f02
-                        <ol>
-                            <li>commit 01</li>
-                            <li>commit 02</li>
-                        </ol>
-                    </li>
-                </ol>
-            </li>
-        </ol>
+    </ol>
     </body>
 </html>
